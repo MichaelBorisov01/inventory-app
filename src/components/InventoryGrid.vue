fc3101ffd933608be175146ee532f8366480dad6
MichaelBorisov01 <bori.mix@yandex.ru>
2024-07-15 14:31:53 +0300

    Добавление предмета инвентаря и сетка для его отображения.

---
+++ src/components/InventoryGrid.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="inventory">
+    <div class="inventory-grid">
+      <InventoryItem v-for="item in items" :key="item.id" :item="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import InventoryItem from './InventoryItem.vue'
+import { useInventoryStore } from '../stores/inventory'
+
+export default {
+  components: { InventoryItem },
+  setup() {
+    const inventoryStore = useInventoryStore()
+    const items = ref(inventoryStore.getItems())
+
+    return { items }
+  }
+}
+</script>
+
+<style lang="scss">
+.inventory {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.inventory-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+}
+</style>
---
+++ src/components/InventoryItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="inventory-item" @click="showDetails = true">
+    {{ item.name }}
+    <ItemDetails v-if="showDetails" :item="item" @close="showDetails = false" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import ItemDetails from './ItemDetails.vue'
+
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    const showDetails = ref(false)
+
+    return { showDetails }
+  }
+}
+</script>
+
+<style lang="scss">
+.inventory-item {
+  background-color: #fff;
+  border: 1px solid #ddd;
+  padding: 10px;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+</style>
