6cedfdd7c78dab3657c1ad26f1bd552d2c05b866
MichaelBorisov01 <bori.mix@yandex.ru>
2024-07-15 14:58:10 +0300

    Добавил главную страницу инвентаря и футер

---
+++ src/components/FooterInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer">
+    <p>Footer text</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.footer {
+  background-color: #f0f0f0;
+  padding: 20px;
+  text-align: center;
+}
+</style>
---
+++ src/pages/InventoryPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="inventory-page">
+    <div class="sidebar">
+      <ItemCard />
+    </div>
+    <div class="inventory">
+      <InventoryGrid />
+    </div>
+    <FooterInfo />
+  </div>
+</template>
+
+<script>
+import ItemCard from '../components/ItemCard.vue'
+import InventoryGrid from '../components/InventoryGrid.vue'
+import FooterInfo from '../components/FooterInfo.vue'
+
+export default {
+  components: { ItemCard, InventoryGrid, FooterInfo }
+}
+</script>
+
+<style lang="scss">
+.inventory-page {
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+}
+
+.sidebar {
+  width: 300px;
+  background-color: #f0f0f0;
+  padding: 20px;
+}
+
+.inventory {
+  flex: 1;
+  padding: 20px;
+}
+</style>
